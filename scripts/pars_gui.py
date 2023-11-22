466aa77386c2562407cd8accaf50767311fc2c08
Airfix982 <25roter52@gmail.com>
2023-11-22 16:47:55 +0400

    fix: gui fixed, prompts work properly despite of register

--- scripts/pars_gui.py
+++ scripts/pars_gui.py
@@ -2,31 +2,35 @@ import tkinter as tk
 from tkinter import messagebox, ttk
 from dependecies import get_parse_data
 
-def on_country_select(event):
-    selected_country = country_var.get()
+def format_string(s):
+    """Форматирование строки с заглавной первой буквой."""
+    return s.capitalize()
+
+def update_city_list(*args):
+    """Обновление списка городов на основе введенной страны."""
+    selected_country = country_var.get().lower()
     cities = country_city_data.get(selected_country, [])
-    city_combobox['values'] = sorted(cities)
-    city_combobox.set('')
+    city_combobox['values'] = [format_string(city) for city in sorted(cities)]
 
 def on_start_button_click(country_city_data):
-    country = country_var.get()
-    city = city_var.get()
-    if country not in country_city_data or (city and city not in country_city_data[country]):
+    country = country_var.get().lower()
+    city = city_var.get().capitalize()
+    if country not in country_city_data or (city and city not in [c.capitalize() for c in country_city_data[country]]):
         messagebox.showerror("Ошибка ввода", "Выбранная страна или город не найдены. Пожалуйста, выберите из списка.")
         return
     # Здесь код для начала сбора данных
 
 if __name__ == "__main__":
-    country_city_data = get_parse_data()
+    country_city_data = {key.lower(): [city.lower() for city in cities] for key, cities in get_parse_data().items()}
     root = tk.Tk()
     root.title("Сбор данных о городах и странах")
     root.geometry("400x200")
 
     ttk.Label(root, text="Выберите страну:").pack()
     country_var = tk.StringVar()
+    country_var.trace("w", update_city_list)
     country_combobox = ttk.Combobox(root, textvariable=country_var)
-    country_combobox['values'] = sorted(country_city_data.keys())
-    country_combobox.bind('<<ComboboxSelected>>', on_country_select)
+    country_combobox['values'] = [format_string(country) for country in sorted(country_city_data.keys())]
     country_combobox.pack()
 
     ttk.Label(root, text="Выберите город:").pack()
@@ -34,7 +38,7 @@ if __name__ == "__main__":
     city_combobox = ttk.Combobox(root, textvariable=city_var)
     city_combobox.pack()
 
-    start_button = tk.Button(root, text="Начать сбор данных", bg="#004158", fg="white", command=lambda:on_start_button_click(country_city_data))
+    start_button = tk.Button(root, text="Начать сбор данных", bg="#004158", fg="white", command=lambda: on_start_button_click(country_city_data))
     start_button.pack(pady=10)
 
     root.mainloop()
